eecf5b43ef49c9ace9084b1ac816743fbf6f9d2f
owengilfellon <owen.gilfellon@opencastsoftware.com>
2021-01-18 12:59:56 +0000

    MDS-435 | Fixed missing import

--- test/unit/uk/gov/hmrc/individualsincomeapi/domain/integrationframework/paye/IfPayeEntrySpec.scala
+++ test/unit/uk/gov/hmrc/individualsincomeapi/domain/integrationframework/paye/IfPayeEntrySpec.scala
@@ -18,7 +18,7 @@ package unit.uk.gov.hmrc.individualsincomeapi.domain.integrationframework.paye
 
 import org.scalatest.{Matchers, WordSpec}
 import play.api.libs.json.Json
-import uk.gov.hmrc.individualsincomeapi.domain.integrationframework.{IfAdditionalFields, IfPayeEntry}
+import uk.gov.hmrc.individualsincomeapi.domain.integrationframework.{IfAdditionalFields, IfPaye, IfPayeEntry}
 import uk.gov.hmrc.individualsincomeapi.domain.integrationframework.IfPayeEntry._
 import utils.IncomePayeHelpers
 
