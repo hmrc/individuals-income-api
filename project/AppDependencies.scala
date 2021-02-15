ae874382d7aabfbdf832e83762dcdc8695cb84a6
parkeradam <30392071+parkeradam@users.noreply.github.com>
2021-02-15 12:10:20 +0000

    MDS-460 : Dependency updates.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -21,10 +21,10 @@ object AppDependencies {
   val compile = Seq(
     ws,
     hmrc                %% "bootstrap-backend-play-26"  % "2.24.0",
-    hmrc                %% "domain"                     % "5.9.0-play-26",
+    hmrc                %% "domain"                     % "5.10.0-play-26",
     hmrc                %% "auth-client"                % "3.0.0-play-26",
     hmrc                %% "play-hal"                   % "2.1.0-play-26",
-    hmrc                %% "play-hmrc-api"              % "4.1.0-play-26",
+    hmrc                %% "play-hmrc-api"              % "5.3.0-play-26",
     hmrc                %% "mongo-caching"              % "6.15.0-play-26",
     hmrc                %% "json-encryption"            % "4.8.0-play-26",
     "com.typesafe.play" %% "play-json-joda"             % "2.9.1"
@@ -34,12 +34,12 @@ object AppDependencies {
     "org.scalatestplus.play" %% "scalatestplus-play"       % "3.1.3"             % scope,
     "org.scalatest"          %% "scalatest"                % "3.0.8"             % scope,
     "org.scalaj"             %% "scalaj-http"              % "2.4.2"             % scope,
-    "org.mockito"            % "mockito-core"              % "3.5.10"             % scope,
+    "org.mockito"            % "mockito-core"              % "3.7.7"             % scope,
     "org.pegdown"            % "pegdown"                   % "1.6.0"             % scope,
     "com.typesafe.play"      %% "play-test"                % PlayVersion.current % scope,
     "com.github.tomakehurst" % "wiremock-jre8"             % "2.27.2"            % scope,
-    hmrc                     %% "reactivemongo-test"       % "4.21.0-play-26"    % scope,
-    hmrc                     %% "service-integration-test" % "0.12.0-play-26"     % scope
+    hmrc                     %% "reactivemongo-test"       % "4.22.0-play-26"    % scope,
+    hmrc                     %% "service-integration-test" % "0.13.0-play-26"     % scope
   )
 
 
